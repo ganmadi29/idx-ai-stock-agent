f7452e26de853136306675cfa643aed9c830eecb
ganmadi29 <62374372+ganmadi29@users.noreply.github.com>
2026-01-30 23:18:13 +0700

    Update telegram.py

--- tools/telegram.py
+++ tools/telegram.py
@@ -1,9 +1,9 @@
 import requests
-from config.settings import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+from config.settings import BOT_TOKEN, CHAT_ID
 
 def send_telegram_message(text):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     requests.post(url, json={
-        "chat_id": TELEGRAM_CHAT_ID,
+        "chat_id": CHAT_ID,
         "text": text
     })
