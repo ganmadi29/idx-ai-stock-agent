3ddc1c2678c10b83e1f7288fe8fd259e6bc9a9bc
ganmadi29 <62374372+ganmadi29@users.noreply.github.com>
2026-02-26 10:52:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from agents.analyst import AnalystAgent
 from agents.narrator import NarratorAgent
 from tools.telegram import send_telegram
 from tools.formatter import format_signal_message
+from datetime import datetime
 
 import gspread
 from google.oauth2.service_account import Credentials
